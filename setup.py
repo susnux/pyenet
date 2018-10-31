0145e6caecf84e0b3534955f7378dd71e6ee5072
Godwhoa <gjdaniel1999@gmail.com>
2018-11-01 03:17:01 +0530

    Download enet sources

--- setup.py
+++ setup.py
@@ -2,25 +2,68 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import os
 import glob
 import sys
 
 source_files = ["enet.pyx"]
 
 _enet_files = glob.glob("enet/*.c")
+lib_version = "1.3.13"
 
 if not _enet_files:
-    print("You need to download and extract the enet 1.3 source to enet/")
-    print("Download the source from: http://enet.bespin.org/Downloads.html")
-    print("See the README for more instructions")
-    sys.exit(1)
+    import tarfile
+    try:
+        import urllib.request as urllib_request
+    except ImportError:
+        import urllib as urllib_request
+
+    enet_dir = "enet-%s/" % lib_version
+    enet_file = "enet-%s.tar.gz" % lib_version
+    enet_url = "http://enet.bespin.org/download/%s" % enet_file
+
+    if not os.path.isfile(enet_file):
+        print("Downloading enet")
+
+        # begin code shamelessly stolen from StackOverflow
+        # http://stackoverflow.com/a/13895723/2730399
+        # Answer by https://stackoverflow.com/users/4279/j-f-sebastian
+        def reporthook(blocknum, blocksize, totalsize):
+            readsofar = blocknum * blocksize
+            if totalsize > 0:
+                percent = readsofar * 1e2 / totalsize
+                s = "\r%5.1f%% %*d / %d" % (percent, len(str(totalsize)),
+                                            readsofar, totalsize)
+                sys.stderr.write(s)
+                if readsofar >= totalsize:  # near the end
+                    sys.stderr.write("\n")
+            else:  # total size is unknown
+                sys.stderr.write("read %d\n" % (readsofar, ))
+
+        urllib_request.urlretrieve(enet_url, enet_file, reporthook)
+        print("Finished downloading enet")
+        # end code shamelessly stolen from StackOverflow
+
+    print("Unpacking enet")
+    tar = tarfile.open(enet_file)
+
+    l = len(enet_dir)
+    members = []
+    for member in tar.getmembers():
+        if member.path.startswith(enet_dir):
+            member.path = member.path[l:]
+            members.append(member)
+
+    tar.extractall('enet', members=members)
+    tar.close()
+    print("Finished unpacking enet")
 
 source_files.extend(_enet_files)
 
 define_macros = [('HAS_POLL', None),
                  ('HAS_FCNTL', None),
                  ('HAS_MSGHDR_FLAGS', None),
-                 ('HAS_SOCKLEN_T', None) ]
+                 ('HAS_SOCKLEN_T', None)]
 
 libraries = []
 
@@ -29,7 +72,8 @@ if sys.platform == 'win32':
     libraries.extend(['ws2_32', 'Winmm'])
 
 if sys.platform != 'darwin':
-    define_macros.extend([('HAS_GETHOSTBYNAME_R', None), ('HAS_GETHOSTBYADDR_R', None)])
+    define_macros.extend([('HAS_GETHOSTBYNAME_R', None),
+                          ('HAS_GETHOSTBYADDR_R', None)])
 
 ext_modules = [
     Extension(
@@ -41,7 +85,7 @@ ext_modules = [
         libraries=libraries)]
 
 setup(
-  name = 'enet',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
+    name='enet',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules
 )
